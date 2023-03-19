50947e4dd5e58ae562130251ba04ee11c4d86fef
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-03-19 16:46:36 +0200

    changed API to spaceX and changed some of the images

--- emiliocarreira/src/Pages/Dashboard.js
+++ emiliocarreira/src/Pages/Dashboard.js
@@ -7,12 +7,12 @@ import axios from "axios";
 
 function Dashboard() {
 
-    document.body.style = 'background: #282c34';
+    document.body.style = 'background-image: url()';
 
     const [apiData, setApiData] =useState([]);
 
     useEffect(() => {
-        axios.get("http://api.cfl.ca/v1/games/players?key=TciUR7bNTu1xvfY5Iq9k6oyve8nt57OT") //API request in Aprove process
+        axios.get("https://api.spacexdata.com/v3/launches") //http://api.cfl.ca/v1/games/players?key=TciUR7bNTu1xvfY5Iq9k6oyve8nt57OT (API request denied)
         .then((Response) => {
             console.log(Response.data);
         })
@@ -25,7 +25,7 @@ function Dashboard() {
     
         <>
         <div id='about' class='mx-3 p-2 my-4'>
-            <img class='my-2' variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Logo_CFL_Luxembourg_%282022%29.svg/1280px-Logo_CFL_Luxembourg_%282022%29.svg.png" style={{ width: '30%', marginLeft: 'auto' }} alt='CFL About Logo' />
+            <img class='my-2' variant="top" src="" style={{ width: '30%', marginLeft: 'auto' }} alt='CFL About Logo' />
             <h5 class='col-md-7 m-auto my-2' style={{ marginLeft: '10%', marginRight: '10%', textAlign: 'left' }}>The Canadian Football League is a professional sports league in Canada. The CFL is the highest level of competition in Canadian football. The league consists of nine teams, each located in a city in Canada. They are divided into two divisions: four teams in the East Division and five teams in the West Division.</h5>
 
         </div>
@@ -35,7 +35,7 @@ function Dashboard() {
                 <div class='col-md-4 mx-auto my-4'>
                     <div>
                         <div class='card-body'id='cardBorder'> 
-                        <a href='/'><img src='https://cf-images.us-east-1.prod.boltdns.net/v1/static/4401740954001/d398335f-54f4-44c3-ae3b-81ba39298168/dcb68382-c7c1-40f0-85d3-92a7a30c18b5/1920x1080/match/image.jpg' alt='Dashboar tab' class='w-100' /></a>
+                        <a href='/'><img src='https://wallpapersmug.com/download/1920x1080/8514e0/SpaceX-Falcon-Heavy-rocket-launch-smoke.jpg' alt='Dashboar tab' class='w-100' /></a>
                         <h5 class='card-title'>Title</h5>
                         </div>
                     </div>
@@ -44,7 +44,7 @@ function Dashboard() {
                 <div class='col-md-4 mx-auto my-4'>
                     <div>
                         <div class='card-body'id='cardBorder'> 
-                        <a href='/Compare'><img src='https://nhfantasystorage.blob.core.windows.net/cflnewshub-storage/2022/12/CFL-Awards-109.png' alt='Dashboar tab' class='w-100' /></a>
+                        <a href='/Compare'><img src='https://preview.redd.it/2k-hd-version-spacex-superheavy-launch-v0-5d90vkgbdhba1.png?width=1920&format=png&auto=webp&s=e7a2e4ab0098e0d076389ad05b597423f03ca3a5' alt='Dashboar tab' class='w-100' /></a>
                         <h5 class='card-title'>Compare</h5>
                         </div>
                     </div>
