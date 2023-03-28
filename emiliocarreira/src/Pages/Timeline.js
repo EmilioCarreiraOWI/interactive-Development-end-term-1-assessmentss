f9a2d4e756a2cea062d62a9630aeaf0ee19764d1
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-03-28 14:50:35 +0200

    API applied in the timeline page
    it works! I only need to style it :100:

--- emiliocarreira/src/Pages/Timeline.js
+++ emiliocarreira/src/Pages/Timeline.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Timeline() {
 
     document.body.style = 'background-color: #2D383A';
 
-    return(
-        <p>Timeline</p>
-    );
+  const [launches, setLaunches] = useState([]); //greate a useState, to get the naming for the variables
+
+  useEffect(() => {
+    axios.get('https://api.spacexdata.com/v3/launches') //axios get function for the API
+      .then(response => setLaunches(response.data))
+      .catch(error => console.log(error)); //if the data do not responed, this function catches the error
+  }, []);
+
+  return (
+    <div className="timeline"> {/*difrant class name for the div 'timeline' */}
+    <verticalTimeline></verticalTimeline>
+      {launches.map(launch => (
+        <div key={launch.flight_number} className="event">
+          <h3>{launch.mission_name}</h3>
+          <p>{launch.launch_date_local}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Timeline;
--- emiliocarreira/src/componets/navbar.js
+++ emiliocarreira/src/componets/navbar.js
@@ -11,7 +11,7 @@ function BasicNav() {
     return(
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/"><img src="https://w7.pngwing.com/pngs/560/188/png-transparent-elon-musk-sticker-telegram-vkontakte-odnoklassniki-musk-stick-sticker-cartoon-odnoklassniki-thumbnail.png" alt="React Logo" id="logoImg"/></Navbar.Brand>
+                <Navbar.Brand href="/"><img src="https://w7.pngwing.com/pngs/560/188/png-transparent-elon-musk-sticker-telegram-vkontakte-odnoklassniki-musk-stick-sticker-cartoon-odnoklassniki-thumbnail.png" alt="React Logo" id="logoImg"/> SpaceX</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Dashboard</Nav.Link>
                     <Nav.Link href="/Compare">Compare</Nav.Link>
